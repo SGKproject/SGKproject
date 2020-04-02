370215c25bc9fc3c911b95b742c6cbb328a42e3f
Danil <SivtsovDT@gmail.com>
2020-04-03 00:47:52 +0300

    Соединил клиент-сервер OPC-UA с основной веткой

---
+++ sgk-project-backend/client.py
@@ -0,0 +1,83 @@
+# coding=utf-8
+from opcua import Client
+import pickle
+import pika
+#from opcua.tools import embed
+
+URL = 'opc.tcp://server:4840/'
+PERIOD = 100
+
+params = {}
+total = []
+channel = ''
+
+conn_params = pika.ConnectionParameters('rabbit', 5672)
+connection = pika.BlockingConnection(conn_params)
+channel = connection.channel()
+
+channel.queue_declare(queue='route_in',
+                      arguments={'x-message-ttl': 60000},
+                      durable=True)
+
+
+class SubHandler(object):
+
+    # пробовал в этом методе брать имя параметра, как node.get_browse_name().name
+    # но таким образом этот метод вообще перестает работать, происходит timeout постоянно
+    # наверно при get_browse_name происходит обращение к серверу, а это дорого
+    # в итоге закинул имена параметров в словарь params - {nodeid, param_name}
+    @staticmethod
+    def datachange_notification(node, val, data):
+        print((params[node.nodeid.Identifier], val))
+        total.append((params[node.nodeid.Identifier], val))
+
+        body= str((params[node.nodeid.Identifier], val))
+        channel.basic_publish(exchange='',
+                              routing_key='route_in',
+                              body=(pickle.dumps(body, 0)).decode(),
+                              properties=pika.BasicProperties(delivery_mode=2))
+    # channel.basic_publish(exchange= '',
+    # body='HI',
+    # properties = pika.BasicProperties(delivery_mode=2))
+
+
+if __name__ == "__main__":
+    client = Client(URL)
+
+    try:
+        client.connect()
+        print('Client connected')
+        root = client.get_root_node()
+
+        # conn_params = pika.ConnectionParameters('rabbit', 5672)
+        # connection = pika.BlockingConnection(conn_params)
+        # channel = connection.channel()
+
+        # channel.queue_declare(queue='in_queue',
+        # arguments={'x-message-ttl':60000},
+        # durable=True)
+
+        # список переменных нужного объекта
+        vars = root.get_children()[0].get_children()[1].get_variables()
+
+        # добавляю в словарь имена параметров
+        for var in vars:
+            params[var.nodeid.Identifier] = var.get_browse_name().Name
+
+        # подписка на изменения среди данных параметров
+        msclt = SubHandler()
+        sub = client.create_subscription(PERIOD, msclt)
+        handle_data = sub.subscribe_data_change(vars)
+        handle_events = sub.subscribe_events()
+
+        while (True):
+            a = 1
+
+        # embed - это терминал, он был в примерах библиотеки opcua вместо while(True)
+        # причем в терминале можно работать с локальными переменными в live режиме
+        # embed()
+        # for h in handle_data:
+        #     sub.unsubscribe(h)
+        # sub.delete()
+    finally:
+        client.disconnect()
---
+++ sgk-project-backend/server.py
@@ -0,0 +1,54 @@
+from opcua import ua
+from opcua import Server
+import datetime
+import time
+import pandas as pd
+from IPython import embed
+
+URL = 'opc.tcp://0.0.0.0:4840/'
+FILE_NAME = 'ActualDataFeb2020.xlsx'
+TIMEOUT = 5
+
+if __name__ == "__main__":
+
+    name = 'OPCUA_SERVER'
+    server = Server()
+    server.set_endpoint(URL)
+    addspace = server.register_namespace(name)
+    node = server.get_objects_node()
+
+    # добавление объекта
+    Param = node.add_object(addspace, 'WATER')
+
+    data = pd.read_excel(FILE_NAME)
+
+    # перевожу в numpy, т.к. возникают проблемы с типами
+    table = data.to_numpy()
+
+    # список  с параметрами(типа 'node') из таблички
+    variables = []
+    for j, param in list(enumerate(data.columns.tolist()))[1:]:
+        variables.append(Param.add_variable(addspace, param, str(table[0, j]) + '/' + table[0, 0]))
+    for var in variables:
+        var.set_writable()
+
+    server.start()
+    print('Server started at {}'.format(URL))
+    # print('Put enter to transfer data')
+    # input()
+
+    for i in range(1, len(data)):
+        for j, var in enumerate(variables):
+            # Данные размещаются в виде строки "значение  / дата", немного костыльно, так как list из 2 элементов
+            # передавать он не может
+            var.set_value(str(table[i, j + 1]) + ' | ' + table[i, 0])
+        print(i, " line sent")
+        time.sleep(TIMEOUT)
+
+    print("Data transferred")
+
+    # вызов терминала IPython, нужен для отладки и чтобы сервер не выключался
+    # отправки всех данных
+    # можно выйти, написав exit
+    embed()
+    server.stop()
